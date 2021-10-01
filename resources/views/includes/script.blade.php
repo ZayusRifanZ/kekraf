b4f5ab37480ee93221c77b7e2aba517896c5e710
ZayusRifanZ <rifanmidai99@gmail.com>
2021-10-02 02:23:23 +0700

    change aos offline storage

--- resources/views/includes/script.blade.php
+++ resources/views/includes/script.blade.php
@@ -1,6 +1,6 @@
 <script src="/vendor/jquery/jquery-3.6.0.min.js"></script>
 <script src="/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+<script src="/vendor/aos@2.3.1/aos.js"></script>
 <script src="/script/navbar-scroll.js"></script>
 <script>
 AOS.init();
--- resources/views/includes/style.blade.php
+++ resources/views/includes/style.blade.php
@@ -1,2 +1,2 @@
-<link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet" />
+<link rel="stylesheet" href="/vendor/aos@2.3.1/aos.css">
 <link rel="stylesheet" href="/style/main.css" />
