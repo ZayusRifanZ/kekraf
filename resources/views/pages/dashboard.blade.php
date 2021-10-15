81b2df1364a217d6e8e4c72ab292f745ea1cb3e3
ZayusRifanZ <rifanmidai99@gmail.com>
2021-10-15 15:23:01 +0700

    migration view dashboard

--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -19,7 +19,7 @@
               <div class="card mb-2">
                 <div class="card-body">
                   <div class="dashboard-card-title">Pelanggan</div>
-                  <div class="dashboard-card-subtitle">15,209</div>
+                  <div class="dashboard-card-subtitle">{{ number_format($customer) }}</div>
                 </div>
               </div>
             </div>
@@ -27,7 +27,7 @@
               <div class="card mb-2">
                 <div class="card-body">
                   <div class="dashboard-card-title">Pendapatan</div>
-                  <div class="dashboard-card-subtitle">Rp 902,000</div>
+                  <div class="dashboard-card-subtitle">Rp {{ number_format($revenue) }}</div>
                 </div>
               </div>
             </div>
@@ -35,7 +35,7 @@
               <div class="card mb-2">
                 <div class="card-body">
                   <div class="dashboard-card-title">Transaksi</div>
-                  <div class="dashboard-card-subtitle">20,020,000</div>
+                  <div class="dashboard-card-subtitle">{{ number_format($transaction_count) }}</div>
                 </div>
               </div>
             </div>
@@ -44,72 +44,32 @@
           <div class="row mt-3">
             <div class="col-12 mt-2">
               <h2 class="section-title">Riwayat Transaksi</h2>
-              <a
-                href="{{ url('/dashboard/transaction/1') }}"
-                class="card card-list d-blok mb-2"
-              >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-md-1">
-                      <img
-                        src="/images/KacaMataAntiRadiasi.jpg"
-                        alt=""
-                        class="w-100"
-                      />
-                    </div>
-                    <div class="col-md-4">Kaca mata anti radiasi</div>
-                    <div class="col-md-3">Gumala Kumar</div>
-                    <div class="col-md-3">12 Januari, 2020</div>
-                    <div class="col-md-1 d-none d-md-block">
-                      <i class="fas fa-arrow-right"></i>
-                    </div>
-                  </div>
-                </div>
-              </a>
-              <a
-                href="{{ url('/dashboard/transaction/1') }}"
-                class="card card-list d-blok mb-2"
-              >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-md-1">
-                      <img
-                        src="/images/AdidasSepatuBasket.jpg"
-                        alt=""
-                        class="w-100"
-                      />
-                    </div>
-                    <div class="col-md-4">Adidas Sepatu Basket</div>
-                    <div class="col-md-3">Grid</div>
-                    <div class="col-md-3">12 Januari, 2020</div>
-                    <div class="col-md-1 d-none d-md-block">
-                      <i class="fas fa-arrow-right"></i>
+
+              @foreach ($transaction_data as $transaction)
+                <a
+                  href="{{ route('dashboard-transaction-detail', $transaction->id) }}"
+                  class="card card-list d-blok mb-2"
+                >
+                  <div class="card-body">
+                    <div class="row">
+                      <div class="col-md-1">
+                        <img
+                          src="{{ Storage::url($transaction->product->galleries->first()->photos ?? '') }}"
+                          alt=""
+                          class="w-100"
+                        />
+                      </div>
+                      <div class="col-md-4">{{ $transaction->product->name ?? '' }}</div>
+                      <div class="col-md-3">{{ $transaction->transaction->user->name ?? '' }}</div>
+                      <div class="col-md-3">{{ $transaction->created_at ?? '' }}</div>
+                      <div class="col-md-1 d-none d-md-block">
+                        <i class="fas fa-arrow-right"></i>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </a>
-              <a
-                href="{{ url('/dashboard/transaction/1') }}"
-                class="card card-list d-blok mb-2"
-              >
-                <div class="card-body">
-                  <div class="row">
-                    <div class="col-md-1">
-                      <img
-                        src="/images/ParfumMyramK.jpg"
-                        alt=""
-                        class="w-100"
-                      />
-                    </div>
-                    <div class="col-md-4">Parfum MyramK</div>
-                    <div class="col-md-3">Weed nim</div>
-                    <div class="col-md-3">12 Januari, 2020</div>
-                    <div class="col-md-1 d-none d-md-block">
-                      <i class="fas fa-arrow-right"></i>
-                    </div>
-                  </div>
-                </div>
-              </a>
+                </a>
+              @endforeach
+
             </div>
           </div>
         </div>
