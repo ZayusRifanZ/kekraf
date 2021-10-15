7e8976ed9d696ebd11c850898970ec63038b0037
ZayusRifanZ <rifanmidai99@gmail.com>
2021-10-15 15:21:01 +0700

    add model transactionDetail

--- app/TransactionDetail.php
+++ app/TransactionDetail.php
@@ -24,4 +24,12 @@ class TransactionDetail extends Model
     protected $hidden = [
         
     ];
+
+    public function transaction(){
+        return $this->hasOne(Transaction::class, 'id', 'transactions_id');
+    }
+
+    public function product(){
+        return $this->hasOne(Product::class, 'id', 'products_id');
+    }
 }
